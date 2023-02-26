1c1a7a5fb1ef17a541887c3f1fda0598b243dc54
Sudher <sudhersingh21@gmail.com>
2023-02-26 13:05:25 +0530

    Initial Configuration

---
+++ src/test/java/com/spangles/orgdepartmentmanagement/OrgDepartmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spangles.orgdepartmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgDepartmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
